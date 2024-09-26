e9ff22357142962116ef4045336a2661e87ea0fd
Akshat <akshat@Akshats-MacBook-Pro.local>
2024-09-26 09:39:07 -0400

    Scopes of Coroutine runBlocking, GlobalScope, coroutineScope

---
+++ app/src/main/java/com/akshat/coroutinesbasic/Scope.kt
@@ -0,0 +1,32 @@
+package com.akshat.coroutinesbasic
+
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+@OptIn(DelicateCoroutinesApi::class)
+fun main(){
+    println("Program execution will now block")
+    runBlocking {
+        launch {
+            delay(1000L)
+            println("task from runBlocking")
+        }
+
+        GlobalScope.launch {
+            delay(500L)
+            println("Task from GlobalScope")
+        }
+
+        coroutineScope {
+            launch {
+                delay(1500L)
+                println("Task from coroutineScope")
+            }
+        }
+    }
+    println("Program execution will now continue")
+}
