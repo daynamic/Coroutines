fa2fdb6b1b4504373346e61edca12613e931e18e
Akshat <akshat@Akshats-MacBook-Pro.local>
2024-10-07 11:17:22 -0400

    applied filter.Kt to donwloaded image to turn it to black and white

--- app/src/main/java/com/akshat/coroutinesbasic/MainActivity.kt
+++ app/src/main/java/com/akshat/coroutinesbasic/MainActivity.kt
@@ -1,20 +1,57 @@
 package com.akshat.coroutinesbasic
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.ImageView
+import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.akshat.coroutinesbasic.ImageProcessingCoroutine.Filter
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
+
+    private val IMAGE_URL = "https://raw.githubusercontent.com/DevTides/JetpackDogsApp/master/app/src/main/res/drawable/dog.png"
+    private val coroutineScope = CoroutineScope(Dispatchers.Main)
+    private lateinit var imageView: ImageView
+    private lateinit var progressBar: ProgressBar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        imageView = findViewById(R.id.imageView)
+        progressBar = findViewById(R.id.progressBar)
+        coroutineScope.launch {
+            val originalDeferred = coroutineScope.async(Dispatchers.IO){
+                getOriginalBitmap()
+            }
+            val originalBitmap = originalDeferred.await()
+
+            val filteredDeferred = coroutineScope.async(Dispatchers.Default) { applyFilter(originalBitmap) }
+
+            val filteredBitmap = filteredDeferred.await()
+
+            loadImage(filteredBitmap)
         }
+
     }
+
+    private fun getOriginalBitmap()=
+        URL(IMAGE_URL).openStream().use {
+            BitmapFactory.decodeStream(it)
+        }
+
+    private fun applyFilter(originalBitmap: Bitmap) = Filter.apply(originalBitmap)
+
+    private fun loadImage(bmp: Bitmap){
+        progressBar.visibility = View.GONE
+        imageView.setImageBitmap(bmp)
+        imageView.visibility = View.VISIBLE
+    }
+
 }
